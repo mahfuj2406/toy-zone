94eda764cad3aa4c7f1696e9f1a06636f9aa4c46
Md. Mahfujur Rahman <mahfujur15-2406@diu.edu.bd>
2023-05-19 17:59:19 +0600

    all toys table rows components added

--- src/pages/AllToys/AllToysRow.jsx
+++ src/pages/AllToys/AllToysRow.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const AllToysRow = () => {
+const AllToysRow = ({ toy }) => {
+    const { _id, toyName, price, sellerName, availableQuantity, subCategory } = toy;
+    console.log(toy)
     return (
-        <>
-        </>
+        <tr className=''>
+            <th className="text-center">
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td className="">
+                <div className="text-start">
+                    <div>
+                        <div className="font-bold">{sellerName}</div>
+                    </div>
+                </div>
+            </td>
+            <td className="text-center">
+                {toyName}
+            </td>
+            <td className="text-center">{subCategory}</td>
+            <td className="text-center">{price}</td>
+            <td className="text-center">{availableQuantity}</td>
+            <th>
+                <button className="btn  btn-outline">View Details</button>
+            </th>
+        </tr>
     );
 };
 
